db28702e5acd1febf67a4a7eef0a35d7f7959fb6
Dai Vo <vokhacminhdai@gmail.com>
2025-07-30 10:02:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,9 @@ def send_email(name, recipient, attendance, guests):
         Hi {name},
 
         Thank you for letting us know you won't be able to attend. 
+
         We understand and will miss you dearly. 
+        
         If your plans change, feel free to RSVP again anytime.
 
         With love,
